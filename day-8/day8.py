87db61d3579d09d43bd0d5922dfce71aaf05ee69
dKustura <denis_kustura@hotmail.com>
2025-12-08 13:18:42 +0100

    Day 8 - with UnionFind

--- day-8/day8.py
+++ day-8/day8.py
@@ -6,11 +6,35 @@ from typing import List, Optional, Tuple
 
 CUTOFF = 1000
 
+class UnionFind:
+    def __init__(self, n):
+        self.parent = list(range(n))
+        self.size = [1] * n
+    
+    def find(self, x):
+        if self.parent[x] != x:
+            self.parent[x] = self.find(self.parent[x])
+            
+        return self.parent[x]
+    
+    def union(self, x, y):
+        px, py = self.find(x), self.find(y)
+        if px == py:
+            return False
+        
+        if self.size[px] < self.size[py]:
+            px, py = py, px
+            
+        self.parent[py] = px
+        self.size[px] += self.size[py]
+        
+        return True
+
 def calc_distance(p1: Tuple[int, int, int], p2: Tuple[int, int, int]):
     x1, y1, z1 = p1
     x2, y2, z2 = p2
     
-    return sqrt((x1 - x2)**2 + (y1 - y2)**2 + (z1 - z2)**2)
+    return (x1 - x2)**2 + (y1 - y2)**2 + (z1 - z2)**2
 
 def connect_junction_boxes(points: List[Tuple[int, int, int]], max_connections: Optional[int]):
     point_count = len(points)
@@ -20,70 +44,23 @@ def connect_junction_boxes(points: List[Tuple[int, int, int]], max_connections: 
     for i in range(point_count):
         for j in range(i + 1, point_count):
             distance = calc_distance(points[i], points[j])
-            pairs.append((i, j, distance))
+            pairs.append((distance, i, j))
     
-    # Sort once instead of inserting in sorted order
-    pairs.sort(key=lambda x: x[2])
+    pairs.sort()
     
-    # circuit ID -> circuit size
-    circuits = {}
+    uf = UnionFind(point_count)
+    groups = point_count
     
-    # points -> circuit ID
-    point_to_circuit = {}
-    
-    circuit_id = 0
-    k = 0
-    
-    while True:
+    for k, (distance, i, j) in enumerate(pairs):
         if max_connections is not None and k >= max_connections:
             break
         
-        i, j, distance = pairs[k]
-        p1 = points[i]
-        p2 = points[j]
-        
-        if p1 in point_to_circuit and p2 in point_to_circuit:
-            # both points are in a circuit, merge them if it's not the same one
-            c1 = point_to_circuit[p1]
-            c2 = point_to_circuit[p2]
-            if c1 != c2:
-                # merge smaller cirtcuit into larger to minimize updates
-                if circuits[c1] < circuits[c2]:
-                    c1, c2 = c2, c1
-                
-                circuits[c1] += circuits[c2]
-                del circuits[c2]
-                
-                # only update points that belong to c2
-                for p, c in point_to_circuit.items():
-                    if c == c2:
-                        point_to_circuit[p] = c1
-                
-        elif p1 in point_to_circuit:
-            # p1 already in a circuit
-            c = point_to_circuit[p1]
-            # add p2 to the circuit of p1
-            point_to_circuit[p2] = c
-            circuits[c] += 1
-        elif p2 in point_to_circuit:
-            # p2 already in a circuit
-            c = point_to_circuit[p2]
-            # add p1 to the circuit of p2
-            point_to_circuit[p1] = c
-            circuits[c] += 1
-        else:
-            # new circuit created
-            circuit_id += 1
-            point_to_circuit[p1] = circuit_id
-            point_to_circuit[p2] = circuit_id
-            circuits[circuit_id] = 2
-            
-        k += 1
-        
-        if max_connections is None and len(circuits) == 1 and list(circuits.values())[0] == point_count:
-            return p1[0] * p2[0]
+        if uf.union(i, j):
+            groups -= 1
+            if max_connections is None and groups == 1:
+                return points[i][0] * points[j][0]
     
-    top_3 = sorted(circuits.values(), reverse=True)[:3]        
+    top_3 = sorted(uf.size, reverse=True)[:3]        
     return top_3[0] * top_3[1] * top_3[2]
 
 def day8_part1():
@@ -101,8 +78,8 @@ def day8_part2():
         return connect_junction_boxes(points, None)
         
         
-# part1 = day8_part1()
-# print("Top 3 circuit sizes multiplied are:", part1)
+part1 = day8_part1()
+print("Top 3 circuit sizes multiplied are:", part1)
 
 
 part2 = day8_part2()
